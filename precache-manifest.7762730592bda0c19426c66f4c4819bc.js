85b559e5a04878c2bde5f48b1018f57ff171f534
murat <murat@murats-iMac.local>
2020-06-21 14:37:56 +0300

    Updates

---
+++ precache-manifest.7762730592bda0c19426c66f4c4819bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce8e4c8bfd7bf82300431b990265b4e",
+    "url": "/maps/index.html"
+  },
+  {
+    "revision": "7345b35e6ed608959edd",
+    "url": "/maps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "922e748f01f3855b3f0e",
+    "url": "/maps/static/js/2.8f789da7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/maps/static/js/2.8f789da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7345b35e6ed608959edd",
+    "url": "/maps/static/js/main.b40d4a20.chunk.js"
+  },
+  {
+    "revision": "51d11a6cdef233611b3a",
+    "url": "/maps/static/js/runtime-main.fb44b25f.js"
+  },
+  {
+    "revision": "ce30cbd6eeccb48f4a700dadf4b40d98",
+    "url": "/maps/static/media/addimage.ce30cbd6.png"
+  }
+]);
