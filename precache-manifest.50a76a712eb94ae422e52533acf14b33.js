754af76ea8a8f7d98e9d6583a6f5647e3a5d24aa
murat <murat@murats-iMac.local>
2020-06-21 14:39:34 +0300

    Updates

---
+++ precache-manifest.50a76a712eb94ae422e52533acf14b33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411039746a942c104cbab3448c0c7ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd2f3ad456ef6f5ce99",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "922e748f01f3855b3f0e",
+    "url": "/static/js/2.8f789da7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8f789da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd2f3ad456ef6f5ce99",
+    "url": "/static/js/main.c6d845a1.chunk.js"
+  },
+  {
+    "revision": "0c98d08f370da4440630",
+    "url": "/static/js/runtime-main.6e239d9c.js"
+  },
+  {
+    "revision": "ce30cbd6eeccb48f4a700dadf4b40d98",
+    "url": "/static/media/addimage.ce30cbd6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maps/precache-manifest.7762730592bda0c19426c66f4c4819bc.js"
+  "/precache-manifest.50a76a712eb94ae422e52533acf14b33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maps/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
